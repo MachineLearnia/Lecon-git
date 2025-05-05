766f70f6ca933c58ade562f91eb618d6df606066
Guillaume <mlearnia@gmail.com>
2025-05-05 10:20:22 +0100

    modification de guillaume

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ print("projet streamlit terminé, pret a merge")
 print("Je vais faire un commit sur antonin1")
 print("Je vais faire un deuxième commit sur antonin1")
 
-def fonction_antonin():
-    print("hello world from antonin")
+def fonction_antonin(name):
+    print("hello world from {name}")
 
 def fonction_guillaume():
     print("hello world")
