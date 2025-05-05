f12487c7646303baac12e1f354b89a3ad4b7ad08
Guillaume <mlearnia@gmail.com>
2025-05-05 10:13:01 +0100

    fonction guillaume

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ print("Je travaille sur ma branche streamlit")
 print("projet streamlit terminé, pret a merge")
 print("Je vais faire un commit sur antonin1")
 print("Je vais faire un deuxième commit sur antonin1")
+
+def fonction_guillaume():
+    print("hello world")
