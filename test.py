797679d1b695eb468e7033edae2109dd081d9a3d
Antonin <antoninlefevre45@icloud.com>
2025-05-05 11:19:19 +0200

    modif antonin

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ print("projet streamlit terminé, pret a merge")
 print("Je vais faire un commit sur antonin1")
 print("Je vais faire un deuxième commit sur antonin1")
 
-def fonction_antonin():
-    print("hello world from antonin")
+def fonction_antonin(age):
+    print(f"hello world from antonin, il a {age} ans")
 
 def fonction_guillaume():
     print("hello world")
